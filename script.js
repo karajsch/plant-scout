3f5de1e75a30c5673ef33f43ef2cd25b7d278ef1
Pere Grinus <gregjleighton@gmail.com>
2020-12-14 08:40:14 -0500

    updated javascript and jquery for ebirds, now for google API

--- script.js
+++ script.js
@@ -1,28 +1,80 @@
 // jQuery ready function to ensure html loads fully before JS is applied
-$( document ).ready( function() {
+$(document).ready(function () {
+        // KEYS
+        // my eBirds API
+        var birdsKey = "kpre85dgbr8q";
 
-})
+        // CORS anywhere 
+        var CORS = "https://cors-anywhere.herokuapp.com/";
 
-// app functionality:
-// WHEN I search for a city
-    // so need a getElementById or in jQuery $("#id")[], this will get HTML DOM data as opposed to getting the HTML element {https://stackoverflow.com/questions/4069982/document-getelementbyid-vs-jquery}
+        // my Trefle API
+        var trefleKey = "XFCEuJcUs4V77OvMQjS1Xdkf01F0p_MnkDhaN5FyUSA";
 
-// THEN I am presented with my city name, facts, and images about local plants [this will come from the NatureServer Explorer REST API]. Syntax: 
-        // $.ajax({
-        //     url: [NatureServer Explorer REST API], 
-        //     method: "GET"
-        // }).then(function(response) {
-        //     console.log(response);
-        // })
+        // eBirds URL
+        var eBirdURL = "https://cors-anywhere.herokuapp.com/api.ebird.org/v2/data/obs/key=kpre85dgbr8q";
 
-        // $.ajax({
-        //     url: [API],
-        //     method: "GET"
-        // })
 
-// WHEN I scroll down the page 
-// THEN I can save profiles to local storage
-// Syntax:
+
+        // WHEN I search for a city
+        // so need a getElementById or in jQuery $("#id")[], this will get HTML DOM data as opposed to getting the HTML element {https://stackoverflow.com/questions/4069982/document-getelementbyid-vs-jquery}
+
+
+        // THEN I am presented with my city name, facts, and images about local plants [Trefle]. Syntax: 
+
+        var plant
+        var plant = "Oak Tree";
+
+        // // Trefle URL
+        var trefleURL = "https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=k-sJ91dZASAK9fMJxEhTacaKeN9cBPDY5nOmcWJsusk&filter[common_name]=" + plant;
+
+        $.ajax({
+                url: trefleURL,
+                method: "GET"
+        }).then(function (response) {
+                console.log(response);
+
+                var tRow = $("<tr>");
+
+                var plantTd = $("<td>").text(response.Plant);
+                var placeTd = $("<td>").text(response.Place);
+
+                tRow.append(plantTd, placeTd);
+
+                $("tbody").append(tRow);
+        });
+
+        // THEN I am presented with facts about local bird life in my area [eBird]. Syntax:
+
+        var bird = "bohwax";
+        var eBirdURL = "https://cors-anywhere.herokuapp.com/https://api.ebird.org/v2/data/obs/PA/recent" + bird;
+
+        $.ajax({
+                url: eBirdURL,
+                method: "GET"
+        }).then(function (birds) {
+                console.log(birds);
+
+                var tRow = $("<tr>");
+
+                var birdTd = $("<td>").text(response.Bird);
+
+                tRow.append(birdTd);
+
+                $("tbody").append(tRow);
+        });
+
+
+        // THEN my searches can be appended to the application page (this goes inside the $.ajax({})). Syntax:
+        // var tRow = $("<tr>");
+
+        // var birdData = $("<td>").text()
+        // var plantData = ("<td>").text()
+
+        // tRow.append(birdData, plantTd)
+
+        // WHEN I scroll down the page 
+        // THEN I can save profiles to local storage
+        // Syntax:
         // window.localStorage
 
         // to SAVE DATA to localStorage:
@@ -34,5 +86,33 @@ $( document ).ready( function() {
         // to REMOVE DATA from localStorage:
         // localStorage.removeItem("key");
 
-// THEN I can also receive recommendations about local parks to visit (this will come from MapQuest and National Park Service API) 
-// 
+        // THEN I can also receive recommendations about local parks to visit (this will come from MapQuest and National Park Service API) 
+        // 
+
+})
+
+// const inputCharacter = document.getElementById("#search-button");
+        // const userSearch = document.getElementById("#search-button");
+
+        // userSearch.addEventListener("keyup", (e) => {
+        // console.log(e.target.value)
+        // });
+
+        // placeSearch({
+        //         key: 'qGB7LA5vtSnOJhSeAeVA9DhAIxt0mUTn',
+        //         container: document.querySelector('#search-input')
+        // });
+
+        // function mySearch(postal_code) {
+        //         fetch('http://www.mapquestapi.com/search/v2/radius?q=' + postal_code + '&appid=' + key)
+        //                 .then(function (resp) {
+        //                         return resp.json()
+        //                 }) // This converts our data to json.
+        //                 .then(function (data) {
+        //                         displayLocation(data);
+        //                 })
+        //                 .catch(function (err) {
+        //                         // catch any errors
+        //                         console.log(err)
+        //                 });
+        // }
