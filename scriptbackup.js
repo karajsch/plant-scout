b882a6914bf507401f2ddd76c8ba66dbc3c1911c
Pere Grinus <gregjleighton@gmail.com>
2020-12-14 13:14:13 -0500

    added more weather functionality, also incorporated display on page of temperature, times for sunrise and sunset, humidity, and visibility

--- scriptbackup.js
+++ scriptbackup.js
@@ -2,8 +2,10 @@
 // jQuery ready function to ensure html loads fully before JS is applied
 $(document).ready(function () {
         // KEYS
+
         // CORS anywhere 
         var CORS = "https://cors-anywhere.herokuapp.com/";
+
         // my Trefle API
         var trefleKey = "XFCEuJcUs4V77OvMQjS1Xdkf01F0p_MnkDhaN5FyUSA";
 
@@ -11,6 +13,8 @@ $(document).ready(function () {
 
 
         // WHEN I search for a city
+        // so need a getElementById or in jQuery $("#id")[], this will get HTML DOM data as opposed to getting the HTML element {https://stackoverflow.com/questions/4069982/document-getelementbyid-vs-jquery}
+
 
         // THEN I am presented with facts, and images about local plants [Trefle]. 
         var plant = "Oak Tree";
@@ -34,19 +38,24 @@ $(document).ready(function () {
                 $("tbody").append(tRow);
         });
 
-        // THEN I am presented with weather facts in my region for my plant scout.
+        // THEN I am presented with weather facts in my region for my plant scout. Syntax:
         var APIKey = "1676721fa532ea2e5a0ab18034cd5a47";
         var queryURL = "https://api.openweathermap.org/data/2.5/onecall?appid=" + APIKey;
         var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey;
 
         $("#search-button").click(citySearch)
+
+        // THEN my searches can be appended to the application page
+
         // search for city in side bar and get data in Console
         function citySearch(e) {
                 if (e) {
                         e.preventDefault();
                 }
+
                 // userInput
                 var city = $("#search-input").val()
+
                 // jQuery Ajax call to another server [request to get current weather data from API]
                 $.ajax({
                         url: currentWeatherURL + "&q=" + city,
@@ -61,52 +70,27 @@ $(document).ready(function () {
                                 
                                 $("#humidity").text("Humidity: " + onecallresponse.current.humidity);
 
+                                $("#wind-speed").text("Wind Speed: " + onecallresponse.current.wind_speed);
+
                                 $("#uv-index").text("Current UV-Index: " + onecallresponse.current.uvi);
 
-                                $("#visibility").text("Visibility: " + onecallresponse.current.visibility);
+                                $("#precipitation").text("Precipitation: " + onecallresponse.current.rain);
 
                                 // convert kelvin to fahrenheit 
                                 var fahrenheit = Math.round((parseFloat(onecallresponse.current.temp) - 273.15) * 1.80 + 32);
+                                console.log(fahrenheit);
 
                                 // display degrees symbol
                                 var degrees = "\u02DA"
                                 $("#temp").text("Current Temperature: " + fahrenheit + degrees + "F");
 
-                                // convert UNIX sunrise time
-                                var unixTimeSunrise = onecallresponse.current.sunrise;
-
-                                var date = new Date(unixTimeSunrise * 1000);
-
-                                var hours = date.getHours();
-                                var minutes = "0" + date.getMinutes();
-
-                                var formattedSunrise = hours + ":" + minutes.substr(-2);
-                                console.log(formattedSunrise);
-
-                                $("#sunrise").text("Sunrise: " + formattedSunrise);
-
-                                // convert UNIX sunset time measurement
-                                var unixTimeSunset = onecallresponse.current.sunset;
-
-                                var date = new Date(unixTimeSunset * 1000);
-
-                                var hours = date.getHours();
-                                var minutes = "0" + date.getMinutes();
-
-                                var formattedTime = hours + ":" + minutes.substr(-2);
-
-                                console.log(formattedTime);
-                                $("#sunset").text("Sunset: " + formattedTime);
-
-
                         })
                 })
 
 
-        // THEN my searches can be appended to the application page
-
         }
 
+        
         // WHEN I scroll down the page 
         // THEN I can save profiles to local storage
         // Syntax:
