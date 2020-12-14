facec805eb58c490d03773df72242b527a4e9d1c
Kara Schmidt <kara.jane.schmidt@gmail.com>
2020-12-14 18:15:45 -0500

    local storage is working and appending to the dom

--- trefle_script.js
+++ trefle_script.js
@@ -7,18 +7,16 @@ function retrieve_plant(plant) {
         url: searchUrl,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
         if (response.data.length > 0) {
             response.data.forEach(plant => {
-                console.log(plant.common_name);
                 var tBody = $("tbody");
                 var tRow = $("<tr>");
                 var td = $("<td>");
-                var saveButton = $("<button class='btn btn-success'>").text("Save");
+                var saveButton = $("<button class='btn btn-success save-btn'>").text("Save");
                 td.append(saveButton);
                 var imagePlant = $("<img class='imagePlant'>").attr("src", plant.image_url ? plant.image_url : "https://marvel-b1-cdn.bc0a.com/f00000000202027/www.wellnesspetfood.com/sites/default/files/styles/blog_feature/public/media/images/happy-cat-blog-cover.jpg?itok=kJhdXlkP");
                 var image_urlTd = $("<td>").append(imagePlant);
-                var common_nameTd = $("<td>").text(plant.common_name);
+                var common_nameTd = $("<td class='common_name'>").text(plant.common_name);
                 var scientific_nameTd = $("<td>").text(plant.scientific_name);
                 var familyTd = $("<td>").text(plant.family);
                 tRow.append(td, common_nameTd, image_urlTd, scientific_nameTd, familyTd);
@@ -33,9 +31,38 @@ function retrieve_plant(plant) {
 }
 
 
-$("#plant-search-button").on("click", () => {
+//WHEN I push the save plant button
+//THEN the name of the plant appears in My Saved Plants
+function get_from_local_storage() {
+    $("#savedPlants").empty()
+    let saved = JSON.parse(localStorage.getItem("saved"))
+    console.log(saved)
+    saved.forEach(plant => {
+        console.log(plant)
+        $("#savedPlants").append(`<li><a href="#">&nbsp;&nbsp${plant}</a></li>`)
+    })
 
-    let searched = $("#plant-search").val()
-    retrieve_plant(searched)
-    document.getElementById("plant-search").value = "";
+}
+
+$(document).ready(() => {
+    get_from_local_storage()
+    $("#plant-search-button").on("click", () => {
+        let searched = $("#plant-search").val()
+        retrieve_plant(searched)
+        document.getElementById("plant-search").value = "";
+    })
+
+    //WHEN I receive my plant results
+    $("table").on("click", ".save-btn", function () {
+        let selected = $(this).parent().siblings("td.common_name")[0].innerText
+        let old = JSON.parse(localStorage.getItem("saved"))
+        old.push(selected)
+
+        localStorage.setItem("saved", JSON.stringify(old))
+        get_from_local_storage()
+    })
+    //THEN I can click on the "save" button for the ones I want to keep
+    //THEN they appear in the "My Saved Plants" div
+    retrieve_plant("rose")
+
 })
